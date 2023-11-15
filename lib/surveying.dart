c6c3b1b416848034f3aa4bab421b19d70bbaf918
snrnapa <shino.satoru@gmail.com>
2023-11-15 23:18:29 +0900

    リストを並べてみた

--- lib/surveying.dart
+++ lib/surveying.dart
@@ -31,19 +31,78 @@ class _ToDoListPageState extends State<Surveying> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text("Surveying v1.0.0"),
-        ),
-        body: ListView.builder(
-          itemCount: bsList.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('${bsList[index]}'),
-            );
-          },
-        ),
-      ),
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: Text("Surveying v1.0.0"),
+          ),
+          body: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text("地点"),
+                    Text("BackSight"),
+                    Text("FrontSight"),
+                    Text("GroundHeight"),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(1),
+                      margin: EdgeInsets.all(10.0),
+                      height: 500,
+                      width: 100,
+                      color: Colors.blue,
+                      // padding: EdgeInsets.all(4),
+                      child: ListView.builder(
+                        padding: const EdgeInsets.all(5.0),
+                        itemCount: bsList.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text('${bsList[index]}'),
+                          );
+                        },
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(1),
+                      margin: EdgeInsets.all(10.0),
+                      height: 500,
+                      width: 100,
+                      color: Colors.blue,
+                      // padding: EdgeInsets.all(4),
+                      child: ListView.builder(
+                        padding: const EdgeInsets.all(5.0),
+                        itemCount: bsList.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text('${bsList[index]}'),
+                          );
+                        },
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(1),
+                      margin: EdgeInsets.all(10.0),
+                      height: 500,
+                      width: 100,
+                      color: Colors.blue,
+                      // padding: EdgeInsets.all(4),
+                      child: ListView.builder(
+                        padding: const EdgeInsets.all(5.0),
+                        itemCount: bsList.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text('${bsList[index]}'),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ])),
     );
   }
 }
