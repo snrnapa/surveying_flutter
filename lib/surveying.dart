e85b9102888436a5b878f10a5041c7c87fc13454
snrnapa <shino.satoru@gmail.com>
2023-11-18 10:56:15 +0900

    入力テキストの計算をするボタンを動的に作成した

--- lib/surveying.dart
+++ lib/surveying.dart
@@ -17,11 +17,9 @@ class _ToDoListPageState extends State<Surveying> {
       List.generate(5, (i) => TextEditingController());
   final List<TextEditingController> _fsControllers =
       List.generate(5, (i) => TextEditingController());
-  final List<TextEditingController> _resultControllers =
-      List.generate(5, (i) => TextEditingController());
-  // final TextEditingController _pointController = TextEditingController();
-  // final TextEditingController _backSightController = TextEditingController();
-  // final TextEditingController _frontSightController = TextEditingController();
+
+  // final results = List<String>.generate(5, (i) => "Item $i");
+  final List<String> results = List<String>.generate(5, (i) => "Item $i");
 
   //FSとBSを合計しGHを算出する
   double CalculateGroundHeight() {
@@ -46,7 +44,7 @@ class _ToDoListPageState extends State<Surveying> {
                 width: 50,
                 height: 500,
                 child: ListView.builder(
-                  itemCount: 5,
+                  itemCount: _bsControllers.length,
                   itemBuilder: (BuildContext context, int index) {
                     return TextField(controller: _bsControllers[index]);
                   },
@@ -59,19 +57,29 @@ class _ToDoListPageState extends State<Surveying> {
                 width: 50,
                 height: 500,
                 child: ListView.builder(
-                  itemCount: 5,
+                  itemCount: _fsControllers.length,
                   itemBuilder: (BuildContext context, int index) {
                     return TextField(controller: _fsControllers[index]);
                   },
                 ),
               ),
               Container(
-                width: 50,
-                height: 500,
+                width: 100,
+                // height: 500,
                 child: ListView.builder(
-                  itemCount: 5,
+                  itemCount: results.length,
                   itemBuilder: (BuildContext context, int index) {
-                    return TextField(controller: _fsControllers[index]);
+                    return TextButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.teal),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        '${results[index]}',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    );
                   },
                 ),
               ),
