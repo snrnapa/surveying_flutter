52a39a84599b4e57c306aa107f0fa4b4927e79ea
snrnapa <shino.satoru@gmail.com>
2023-11-24 21:54:32 +0900

    サンプルコードの不要な記述を削除した

--- lib/main.dart
+++ lib/main.dart
@@ -32,14 +32,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -94,29 +86,6 @@ class _MyHomePageState extends State<MyHomePage> {
             )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-class MyHomePageBody extends StatelessWidget {
-  MyHomePageBody({Key? key, this.counter}) : super(key: key);
-  final int? counter;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const Text('You have pushed the button this many times:'),
-          Text('$counter', style: Theme.of(context).textTheme.headline4),
-        ],
       ),
     );
   }
