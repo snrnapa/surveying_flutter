95c3c95ad551fbd48184b77c821a5604554febf4
snrnapa <shino.satoru@gmail.com>
2023-12-11 21:07:34 +0900

    paddingを実装

--- lib/components/card_template.dart
+++ lib/components/card_template.dart
@@ -10,40 +10,43 @@ class CardTemplate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: EdgeInsets.all(5),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      color: Colors.green[100],
-      child: Row(
-        children: [
-          Container(
-            width: 250,
-            // color: Colors.black,
-            alignment: Alignment.centerLeft,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ListTile(
-                  title: Text('No.${result['id']}   ${result['scene_name']}'),
-                  subtitle: Text('Note:${result['scene_note']}'),
+        margin: EdgeInsets.all(5),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        color: Colors.green[100],
+        child: Padding(
+          padding: EdgeInsets.only(left: 16),
+          child: Row(
+            children: [
+              Container(
+                width: 230,
+                // color: Colors.black,
+                alignment: Alignment.centerLeft,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ListTile(
+                      title:
+                          Text('No.${result['id']}   ${result['scene_name']}'),
+                      subtitle: Text('Note:${result['scene_note']}'),
+                    ),
+                    Text(
+                        style: TextStyle(fontSize: 13, color: Colors.black),
+                        "担当者 : Napa"),
+                    Text(
+                        style: TextStyle(fontSize: 13, color: Colors.black),
+                        "Upd : ${result['upd_date']}"),
+                  ],
                 ),
-                Text(
-                    style: TextStyle(fontSize: 13, color: Colors.black),
-                    "担当者 : Napa"),
-                Text(
-                    style: TextStyle(fontSize: 13, color: Colors.black),
-                    "LastUpdDate : ${result['upd_date']}"),
-              ],
-            ),
+              ),
+              Container(
+                  padding: EdgeInsets.all(8),
+                  width: 150,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset('appImage/kushima.jpg'),
+                  ))
+            ],
           ),
-          Container(
-              padding: EdgeInsets.all(8),
-              width: 150,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.asset('appImage/kushima.jpg'),
-              ))
-        ],
-      ),
-    );
+        ));
   }
 }
